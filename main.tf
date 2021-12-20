2987d4696015ebad9a55500ed079cc0340109866
guaroc <beyond.waterloo@yahoo.com>
2021-12-20 23:06:36 +0800

    Modified for local envrionment

--- main.tf
+++ main.tf
@@ -1,11 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
-module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name        = var.name
-  region = var.region
-  prefix = var.prefix
-  version = "1.0.0"
+module "ec2-private" {
+  source  = "app.terraform.io/example-org-68fb71/ec2-private/aws"
+  version = "1.0.6"
+  
+  prefix            = "digory"
+  region_name       = var.region
+  ec2_instance_type = "t2.micro"
+  ec2_ami           = "ami-024221a59c9020e72"
+  subnet_id         = "subnet-096dc1d1e7fccabe2"
+  vpc_id            = "vpc-0d58cc6e5455b9c7a"
+  vpc_cidr_blocks   = "173.26.0.0/16"
+
+  tags = {
+    "Project"     = "Demo"
+    "Purpose"     = "Demonstrate module in Terraform"
+    "Envrionment" = "DEV"
+  }  
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "website_endpoint" {
-  value = module.s3-webapp.endpoint
+output "ec2_instance_id" {
+  value = module.ec2-private.instance_id
 }
